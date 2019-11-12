a375794ac0551304c4581f261f38ddabda50e23a
AgathaP <ebrumaktub@gmx.com>
2019-11-12 16:28:28 -0300

    ejercicio 2 resuelto

--- 2-baby-steps.js
+++ 2-baby-steps.js
@@ -1,2 +1,11 @@
-let input = process.argv[2];
-console.log(input);
+let input = process.argv;
+
+const sum = (num) => {
+    let result = 0;
+    for(let i = 2; i < num.length; i++){
+        result += parseInt(num[i]);
+   }
+    return result;
+}
+
+console.log(sum(input));
