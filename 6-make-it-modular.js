b2938932512358196e94470f258f0dccc82efbe4
AgathaP <ebrumaktub@gmx.com>
2019-11-15 11:28:22 -0300

    Reformulo funcion dentro de mymodule y llamado a esa función desde make-it-modular, agrego comentarios. y subo el ejercicio 6 terminado.

--- 6-make-it-modular.js
+++ 6-make-it-modular.js
@@ -1,17 +1,19 @@
 //controla errores y aciertos, enviando un mensaje para informar sobre ello
-//Envía la información a la consola
+//Envía la información a la terminal
+
 const mymodule = require ('./6-mymodule');
 const route = process.argv[2];
 const ext = process.argv[3];
 
-function  gg (callback) {
-    (err, data) => {
-        if(err){
-            return callback(err);
-        }
-        //more code
-       return callback(null, data);
+//Esta es la invocación de la función hecha en mymodule.js.
+mymodule(route, ext, (err, data) => {
+    //Control de errores.
+    if(err){
+        return console.log('hubo un error: ', err);
     }
-};
 
-console.log(gg(route,ext))
+    //Recorro el array "files" hecho en mymodule.js, para finalmente mostrarlo en la terminal.
+    for (let i = 0; i < data.length; i++) {
+           console.log(data[i]);
+    }
+})
