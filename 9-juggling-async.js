b140b808c4a737fc0f7aa2193b15cfc34f4b2acc
AgathaEloisa <ebrumaktub@gmx.com>
2025-12-29 15:20:47 -0300

    9 completed

--- 9-juggling-async.js
+++ 9-juggling-async.js
@@ -1,19 +1,26 @@
 const bl = require('bl');
 const http = require('http');
+const async = require("async");
 
-let url1 = process.argv[2];
-let url2 = process.argv[3];
-let url3 = process.argv[4];
+let urls = [
+    process.argv[2], 
+    process.argv[3], 
+    process.argv[4]
+];
+let results = [];
 
-http.get(url1,url2,url3, function (response) {
-    response.pipe(bl(function (err, data) {
-        if (err) { console.error(err); return; }
-        data = data.toString();
-        console.log('url1',data[0]);
-        console.log('url2',data[1]);
-        console.log('url3',data[2]);
+async.forEachOf(urls, (url, index, callback) => {
+    http.get(url, (res) => {
+    res.pipe(bl((err, data) => {
+        if (err) return callback(err);
+        results[index] = data.toString();
+        callback(); 
+    }    ))
+}).on('error',callback);
+}, (err) => {
+    if (err) {
+        console.error(`error: ${err.message}`);
+    } else {
+        results.forEach(result => console.log(result));
     }
-    ));
-}   ).on('error', function (e) {
-    console.error(`Got error: ${e.message}`);
 });
